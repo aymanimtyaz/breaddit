9b7d0e313514a4210a1d06e9c580872c7e0c7662
ayman <aymanimtyaz@gmail.com>
2021-03-27 12:51:32 +0400

    Fix sliding behaviour of the sliding element in the sidebar

--- index.js
+++ index.js
@@ -6,27 +6,26 @@ $(document).ready(function(){
         AD_COUNT++;
         var btn = this
         $(btn).parent().addClass('remove-ad')
+        set_slider_position()
         if (AD_COUNT < 3){
             setTimeout(function() {
                 $(btn).parent().removeClass('remove-ad')
+                set_slider_position()
             }, 5000)
         }
         
     });
 
-
-
-
-    $(window).scroll(function() { 
-        var slider_block = document.getElementById('sidebar-slider')
-        var content_area_height = $('.content-area').height();
-        var last_main_content_item_height = $('#last-main-content-item').height();
-        var top = content_area_height - (last_main_content_item_height + 25) - 50;
+    function set_slider_position() {
+        var slider_block_height = $(slider_block).height();
+        var content_area_height = $(content_area).height();
+        
+        var top = content_area_height - (slider_block_height + 75);
         // console.log('Content Area: '+content_area_height);
         // console.log('Last Main Content Item: '+last_main_content_item_height);
         var scrollPosition = $(this).scrollTop();
         // console.log(scrollPosition)
-        if (scrollPosition >= top+75){
+        if (scrollPosition >= top + 75){
             $(slider_block).removeClass('fix-to-viewport');
             $(slider_block).attr('style', 'position: absolute; top: '+top+'px;');
         }else if (scrollPosition >= 188){
@@ -35,6 +34,12 @@ $(document).ready(function(){
         }else{
             $(slider_block).removeClass('fix-to-viewport');
         }
-    });
- 
+    }
+
+    var slider_block = document.getElementById('sidebar-slider');
+    var content_area = document.getElementById('content-area');
+
+
+    $(window).scroll(set_slider_position)
+
 });
